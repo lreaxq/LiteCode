da3c69c5a9d7df603dee84d21dc7d7e6dfae38c9
lreaxq <141836541+lreaxq@users.noreply.github.com>
2024-01-28 18:46:10 +0300

    Update and rename Updater.lc to Updater.bat

---
+++ files/Updater.bat
@@ -0,0 +1,30 @@
+@echo off
+color 07
+cls
+if /I not "%cd%" EQU "C:\Windows\System32" (
+    echo Guncelleme isleminin devam etmesi icin Lutfen guncelleme dosyasini Yonetici olarak baslatin!
+    powershell -Command "Start-Process -FilePath \"%~dp0%~n0\" -Verb RunAs"
+    exit
+)
+set "cdd=C:\Program Files (x86)\"
+mkdir liteCode
+set "cdd=C:\Program Files (x86)\liteCode\"
+cd %cdd%
+echo LiteCode Derleyiciniz guncelleniyor...
+del "litecode.exe"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/lreaxq/LiteCode/raw/main/files/runner/litecode.exe', 'C:\Program Files (x86)\liteCode\litecode.exe')"
+echo Derleyiniz Guncellendi!
+echo LiteShell Guncelleniyor...
+del "%USERPROFILE%\Desktop\litecode.lc"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/lreaxq/LiteCode/main/files/shell/litecode.lc', '%USERPROFILE%\Desktop\litecode.lc')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/lreaxq/LiteCode/main/files/shell/litecode.lc', 'litecode.lc')"
+echo Shell Guncellendi!
+echo Son ayarlamalar yapiliyor..
+echo derleyici windows'a tanitiliyor...
+set "lcPath=%cd%\litecode.exe"  
+echo litecode uzantisi windows'a tanitiliyor...
+reg add HKEY_CURRENT_USER\Software\Classes\.lc /ve /d "litecodefile" /f
+reg add HKEY_CURRENT_USER\Software\Classes\litecodefile\shell\open\command /ve /d "\"%lcPath%\" \"%%1\"" /f
+echo ".lc dosyasi icin kayit defteri girdisi olusturuldu."
+echo LiteCode Basariyla Guncellendi!
+pause
